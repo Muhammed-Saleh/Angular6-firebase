c4a94c73539f8d76d461409acd788781cb65b96e
Muhammed-Saleh <hamada500005@gmail.com>
2018-08-30 15:06:26 +0300

    start creating the route and component addSkills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddSkillComponent } from './add-skill/add-skill.component';
+
+import { RouterModule, Routes  } from '@angular/router'
+
+const routes:Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'addSkill', component: AddSkillComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AddSkillComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
